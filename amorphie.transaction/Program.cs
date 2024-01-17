07f2080d4b2dae089d60084c7db38b118e3ac504
selimcankacar <71877746+selimcankacar@users.noreply.github.com>
2024-01-17 14:38:39 +0300

    Update Program.cs

--- amorphie.transaction/Program.cs
+++ amorphie.transaction/Program.cs
@@ -2,7 +2,7 @@ using amorphie.transaction.data;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddEnvironmentVariables();
-
+System.Threading.Thread.Sleep(5000);
 await builder.Configuration.AddVaultSecrets(builder.Configuration["DAPR_SECRET_STORE_NAME"],new string[]{"DatabaseConnections","ServiceConnections"});
 
 
